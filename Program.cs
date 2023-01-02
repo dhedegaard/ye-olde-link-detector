efa262cdb6a6997b12dddfb929f87cf202d91423
Dennis Hedegaard <dennis@dhedegaard.dk>
2023-01-02 23:45:25 +0100

    initial work, nothing works yet

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Discord.Net;
+using Discord.WebSocket;
+
+var TOKEN = Environment.GetEnvironmentVariable("TOKEN");
+if (string.IsNullOrWhiteSpace(TOKEN))
+{
+  throw new Exception("Missing TOKEN environment variable.");
+}
+
+var client = new DiscordSocketClient();
+
+client.CurrentUserUpdated += (oldUser, newUser) =>
+{
+  Console.WriteLine($"User {oldUser.Username}#{oldUser.Discriminator} changed their username to {newUser.Username}#{newUser.Discriminator}");
+  return Task.CompletedTask;
+};
+client.GuildAvailable += guild =>
+{
+  Console.WriteLine($"Guild {guild.Name} is available.");
+  return Task.CompletedTask;
+};
+
+await client.LoginAsync(Discord.TokenType.Bot, TOKEN);
+await client.StartAsync();
+await Task.Delay(-1);
