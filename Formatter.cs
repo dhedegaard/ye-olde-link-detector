468ae1b83782bdd4d755c00d6303f0dc133dec53
Dennis Hedegaard <dennis@dhedegaard.dk>
2023-01-14 20:43:55 +0100

    formatter

---
+++ Formatter.cs
@@ -0,0 +1,5 @@
+public static class Formatter
+{
+  public static string FormatOutputMessage(string userId, string url, string postCount, Message firstTimePosted) => $@"
+    🚨🚨🚨**OLD**🚨🚨🚨: <@!{userId}> The URL: <{url}> has previously been posted **${postCount}** time(s) before. 🚨🚨🚨 It was first posted by **{firstTimePosted.AuthorName}**, **${firstTimePosted.Timestamp}** ago".Trim();
+}
