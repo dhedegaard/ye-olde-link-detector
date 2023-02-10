4a824385bd33e779c3df8314cfa6f6ede5e7bd15
Dennis Hedegaard <dennis@dhedegaard.dk>
2023-02-11 00:01:50 +0100

    avoid opening db contexts per guild*channel, just do it per guild during initial loading

--- src/InitialGuildImporter.cs
+++ src/InitialGuildImporter.cs
@@ -1,77 +1,95 @@
 using Discord.WebSocket;
+using Microsoft.EntityFrameworkCore;
 
 namespace YeOldeLinkDetector
 {
   public static class InitialGuildImporter
   {
-    public static void Import(SocketGuild guild)
+    public static async Task Import(SocketGuild guild)
     {
+      using var db = new DataContext();
       foreach (var channel in guild.TextChannels)
       {
-        _ = Task.Run(async () =>
+        var guildElem = await db.Guilds
+          .Where(e => e.GuildId == guild.Id.ToString())
+          .FirstOrDefaultAsync();
+
+        try
         {
-          try
+          ulong? lastMessageId = null;
+          var hasAtLeastOneMessage = false;
+          do
           {
-            ulong? lastMessageId = null;
-            var hasAtLeastOneMessage = false;
-            do
+            Console.WriteLine($"  processing message chunks for guild ({guild.Name}) - channel ({channel.Name}) - lastMessageId: {lastMessageId}");
+            var messageIds = new HashSet<ulong>();
+            await foreach (var chunk in
+              lastMessageId == null || !lastMessageId.HasValue
+                ? channel.GetMessagesAsync()
+                : channel.GetMessagesAsync(dir: Discord.Direction.Before, fromMessageId: lastMessageId.Value)
+            )
             {
-              Console.WriteLine($"  processing message chunks for guild ({guild.Name}) - channel ({channel.Name}) - lastMessageId: {lastMessageId}");
-              var messageIds = new HashSet<ulong>();
-              await foreach (var chunk in
-                lastMessageId == null || !lastMessageId.HasValue
-                  ? channel.GetMessagesAsync()
-                  : channel.GetMessagesAsync(dir: Discord.Direction.Before, fromMessageId: lastMessageId.Value)
-              )
+              // TODO: If all the messages in the chunk is already known,
+              // stop fetching chunks as we probably have all the messages.
+              foreach (var message in chunk)
               {
-                // TODO: If all the messages in the chunk is already known,
-                // stop fetching chunks as we probably have all the messages.
-                foreach (var message in chunk)
+                if (guildElem != null && message.Id.ToString() == guildElem.LastMessageId)
                 {
-                  messageIds.Add(message.Id);
-                  hasAtLeastOneMessage = true;
-                  if (message.Author.IsBot || string.IsNullOrWhiteSpace(message.Content))
-                  {
-                    continue;
-                  }
-                  foreach (var url in FindUrlsInContent.FindUrls(message.Content))
-                  {
-                    using var db = new DataContext();
-
-                    var existing = await db.FindAsync<Message>(message.Id.ToString());
-                    if (existing == null)
-                    {
-                      await db.AddAsync(
-                        new Message(
-                          MessageId: message.Id.ToString(),
-                          Url: url,
-                          ChannelId: message.Channel.Id.ToString(),
-                          Timestamp: message.CreatedAt,
-                          AuthorName: message.Author.Username
-                        )
-                      );
-                    }
-                    await db.SaveChangesAsync();
-                  }
+                  Console.WriteLine($"  hit LastMessageId for guild: {guild.Name}");
+                  return;
                 }
-                var lowestMessageId = messageIds.Min();
-                lastMessageId = lastMessageId.HasValue && lastMessageId.Value == lowestMessageId
-                  ? null
-                  : lowestMessageId;
-                Console.WriteLine($"  done processing chunk for guild ({guild.Name}) - channel ({channel.Name}) - chunk: {chunk.Count} - lastMessageId: {lastMessageId}");
-              }
-            } while (hasAtLeastOneMessage && lastMessageId.HasValue);
-            Console.WriteLine("no more messages for channel: " + channel.Name + " (" + channel.Id + ")");
-          }
-          catch (Discord.Net.HttpException e)
-          {
-            if (((int)e.HttpCode) == 50001)
-            {
-              Console.WriteLine("Missing permission to read channel " + channel.Name + " (" + channel.Id + ")");
-            }
-          }
 
-        });
+                // If there's no guild element, or the message is newer than the last message
+                // we've seen, update the guild element for the next boot.
+                if (guildElem == null || guildElem.LastMessageTimestamp < message.CreatedAt)
+                {
+                  guildElem = new Guild(
+                    GuildId: guild.Id.ToString(),
+                    LastMessageId: message.Id.ToString(),
+                    LastMessageTimestamp: message.CreatedAt);
+                  await db.AddAsync(guildElem);
+                }
+
+                messageIds.Add(message.Id);
+                hasAtLeastOneMessage = true;
+                if (message.Author.IsBot || string.IsNullOrWhiteSpace(message.Content))
+                {
+                  continue;
+                }
+                foreach (var url in FindUrlsInContent.FindUrls(message.Content))
+                {
+                  var existing = await db.FindAsync<Message>(message.Id.ToString());
+                  if (existing == null)
+                  {
+                    await db.AddAsync(
+                      new Message(
+                        MessageId: message.Id.ToString(),
+                        Url: url,
+                        ChannelId: message.Channel.Id.ToString(),
+                        Timestamp: message.CreatedAt,
+                        AuthorName: message.Author.Username
+                      )
+                    );
+                  }
+                  await db.SaveChangesAsync();
+                }
+              }
+              var lowestMessageId = messageIds.Min();
+              lastMessageId = lastMessageId.HasValue && lastMessageId.Value == lowestMessageId
+                ? null
+                : lowestMessageId;
+              Console.WriteLine($"  done processing chunk for guild ({guild.Name}) - channel ({channel.Name}) - chunk: {chunk.Count} - lastMessageId: {lastMessageId}");
+            }
+          } while (hasAtLeastOneMessage && lastMessageId.HasValue);
+          Console.WriteLine("no more messages for channel: " + channel.Name + " (" + channel.Id + ")");
+        }
+        catch (Discord.Net.HttpException e)
+        {
+          if (((int)e.HttpCode) == 50001)
+          {
+            Console.WriteLine("Missing permission to read channel " + channel.Name + " (" + channel.Id + ")");
+          }
+        }
+
       }
     }
   }
