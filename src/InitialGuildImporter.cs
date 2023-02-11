f898bd76e928a92e81ea2c5d47c37fcfc07f6b5a
Dennis Hedegaard <dennis@dhedegaard.dk>
2023-02-11 21:19:30 +0100

    split the logic for processing messages and the logic for fetching chunks of messages.

--- src/InitialGuildImporter.cs
+++ src/InitialGuildImporter.cs
@@ -5,6 +5,41 @@ namespace YeOldeLinkDetector
 {
   public static class InitialGuildImporter
   {
+    private static async IAsyncEnumerable<Discord.IMessage> GetAllNonEmptyNonBotMessagesAsync(SocketTextChannel channel)
+    {
+      var hasAtLeastOneMessage = false;
+      ulong? lastMessageId = null;
+      do
+      {
+        await foreach (var chunk in
+          lastMessageId == null || !lastMessageId.HasValue
+            ? channel.GetMessagesAsync()
+            : channel.GetMessagesAsync(dir: Discord.Direction.Before, fromMessageId: lastMessageId.Value)
+        )
+        {
+          var messageIds = new HashSet<ulong>();
+          foreach (var message in chunk.Where(message => !message.Author.IsBot && !string.IsNullOrWhiteSpace(message.Content)))
+          {
+            hasAtLeastOneMessage = true;
+            messageIds.Add(message.Id);
+            yield return message;
+          }
+
+          if (!messageIds.Any())
+          {
+            // No messages in the current chunk, stop fetching messages for
+            // the given channel.
+            yield break;
+          }
+
+          var lowestMessageId = messageIds.Min();
+          lastMessageId = lastMessageId.HasValue && lastMessageId.Value == lowestMessageId
+            ? null
+            : lowestMessageId;
+        }
+      } while (hasAtLeastOneMessage && lastMessageId.HasValue);
+    }
+
     public static async Task Import(SocketGuild guild)
     {
       using var db = new DataContext();
@@ -12,67 +47,43 @@ namespace YeOldeLinkDetector
       {
         try
         {
-          ulong? lastMessageId = null;
-          var hasAtLeastOneMessage = false;
-          do
+          Console.WriteLine($"    processing messages for guild ({guild.Name}) - channel ({channel.Name})");
+          var added = 0;
+          await foreach (var message in GetAllNonEmptyNonBotMessagesAsync(channel))
           {
-            Console.WriteLine($"    processing message chunks for guild ({guild.Name}) - channel ({channel.Name}) - lastMessageId: {lastMessageId}");
-            var messageIds = new HashSet<ulong>();
-            await foreach (var chunk in
-              lastMessageId == null || !lastMessageId.HasValue
-                ? channel.GetMessagesAsync()
-                : channel.GetMessagesAsync(dir: Discord.Direction.Before, fromMessageId: lastMessageId.Value)
-            )
+            foreach (var url in FindUrlsInContent.FindUrls(message.Content))
             {
-              foreach (var message in chunk)
+              var existing = await db.Messages.FirstOrDefaultAsync(e => e.MessageId == message.Id.ToString() && e.Url == url);
+              if (existing != null)
               {
-                messageIds.Add(message.Id);
-                hasAtLeastOneMessage = true;
-                if (message.Author.IsBot || string.IsNullOrWhiteSpace(message.Content))
-                {
-                  continue;
-                }
-                foreach (var url in FindUrlsInContent.FindUrls(message.Content))
-                {
-                  var existing = await db.Messages.FirstOrDefaultAsync(e => e.MessageId == message.Id.ToString() && e.Url == url);
-                  if (existing != null)
-                  {
-                    Console.WriteLine($"  Stopping load for guild/channel ({guild.Name} // {channel.Name}) as message with ID + URL already exists: {message.Id} - {url}");
-                    return;
-                  }
-                  await db.AddAsync(
-                    new Message(
-                      MessageId: message.Id.ToString(),
-                      Url: url,
-                      ChannelId: message.Channel.Id.ToString(),
-                      Timestamp: message.CreatedAt,
-                      AuthorName: message.Author.Username
-                    )
-                  );
-                }
+                Console.WriteLine($"  Stopping load for guild/channel ({guild.Name} // {channel.Name}) as message with ID + URL already exists: {message.Id} - {url}");
+                return;
               }
-              await db.SaveChangesAsync();
-
-              if (!messageIds.Any())
+              await db.AddAsync(
+                new Message(
+                  MessageId: message.Id.ToString(),
+                  Url: url,
+                  ChannelId: message.Channel.Id.ToString(),
+                  Timestamp: message.CreatedAt,
+                  AuthorName: message.Author.Username
+                )
+              );
+              added++;
+              if (added % 50 == 0)
               {
-                // No messages in the current chunk, stop fetching messages for
-                // the given channel.
-                break;
-              }
+                Console.WriteLine($"    Added {added} messages for channel: {channel.Name} ({channel.Id})");
 
-              var lowestMessageId = messageIds.Min();
-              lastMessageId = lastMessageId.HasValue && lastMessageId.Value == lowestMessageId
-                ? null
-                : lowestMessageId;
+              }
             }
-          } while (hasAtLeastOneMessage && lastMessageId.HasValue);
-          Console.WriteLine("no more messages for channel: " + channel.Name + " (" + channel.Id + ")");
+          }
+          await db.SaveChangesAsync();
+          Console.WriteLine("  No more messages for channel: " + channel.Name + " (" + channel.Id + ")");
         }
         catch (Discord.Net.HttpException e)
         {
           if (((int)e.HttpCode) == 50001)
           {
-            Console.WriteLine("Missing permission to read channel " + channel.Name + " (" + channel.Id + ")");
+            Console.WriteLine("  Missing permission to read channel " + channel.Name + " (" + channel.Id + ")");
           }
         }
 
