9050664eae9d2fac14224dd318301f7d6c49b180
Dennis Hedegaard <dennis@dhedegaard.dk>
2025-02-17 16:43:09 +0100

    Do not keep Url as Uri type

--- YeOldeLinkDetector.Data/src/Message.cs
+++ YeOldeLinkDetector.Data/src/Message.cs
@@ -7,7 +7,11 @@ namespace YeOldeLinkDetector.Data;
 public record Message(
   [property: Key]
   string MessageId,
+#pragma warning disable CA1054 // URI-like parameters should not be strings
+#pragma warning disable CA1056 // URI-like properties should not be strings
   string Url,
+#pragma warning restore CA1056 // URI-like properties should not be strings
+#pragma warning restore CA1054 // URI-like parameters should not be strings
   string ChannelId,
   DateTimeOffset Timestamp,
   string AuthorName
