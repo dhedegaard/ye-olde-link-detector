74f32aea9934b52f0eba71c881cbc953d1cdd62c
Dennis Hedegaard <dennis@dhedegaard.dk>
2025-03-15 12:10:45 +0100

    more slight formatting

--- YeOldeLinkDetector.Bot/src/ConfigurationService.cs
+++ YeOldeLinkDetector.Bot/src/ConfigurationService.cs
@@ -8,13 +8,8 @@ internal sealed class ConfigurationService()
   private readonly string? _token = Environment.GetEnvironmentVariable("TOKEN");
   public string Token
   {
-    get
-    {
-      if (string.IsNullOrWhiteSpace(_token))
-      {
-        throw new InvalidOperationException("Missing TOKEN environment variable.");
-      }
-      return _token;
-    }
+    get => string.IsNullOrWhiteSpace(_token)
+      ? throw new InvalidOperationException("Missing TOKEN environment variable.")
+      : _token;
   }
 }
