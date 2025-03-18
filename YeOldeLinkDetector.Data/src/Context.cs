948d6f9e979eddc6fbeb0e3bdae201cbfa4d2462
Dennis Hedegaard <dennis@dhedegaard.dk>
2025-03-18 21:46:39 +0100

    improved pattern matching

--- YeOldeLinkDetector.Data/src/Context.cs
+++ YeOldeLinkDetector.Data/src/Context.cs
@@ -11,12 +11,11 @@ public class DataContext(ILogger<DataContext> logger) : DbContext
   private static readonly Action<ILogger, string, Exception?> _logWarning =
     LoggerMessage.Define<string>(LogLevel.Warning, new EventId(0, "EFCore"), "{Message}");
 
-  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-  {
-    var connStr = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
     optionsBuilder
       .UseNpgsql(
-        !string.IsNullOrWhiteSpace(connStr)
+        Environment.GetEnvironmentVariable("CONNECTION_STRING") is string connStr
+        && !string.IsNullOrWhiteSpace(connStr)
           ? connStr
           : new NpgsqlConnectionStringBuilder
           {
@@ -29,5 +28,4 @@ public class DataContext(ILogger<DataContext> logger) : DbContext
       .EnableDetailedErrors()
       .LogTo(message => _logWarning(logger, message, null), LogLevel.Warning)
       .EnableThreadSafetyChecks();
-  }
 }
