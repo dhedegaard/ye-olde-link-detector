108dabef0a6708fb30bba0ea52305a5a8cae2dfc
Dennis Hedegaard <dennis@dhedegaard.dk>
2025-11-13 18:12:45 +0100

    replaced serilog with standard logging, to avoid excessive dependencies

--- YeOldeLinkDetector.Bot/src/Program.cs
+++ YeOldeLinkDetector.Bot/src/Program.cs
@@ -1,18 +1,17 @@
-﻿using System.Globalization;
-using Serilog;
-using YeOldeLinkDetector.Bot;
+﻿using YeOldeLinkDetector.Bot;
 using YeOldeLinkDetector.Data;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
 builder
   .Logging.ClearProviders()
-  .AddSerilog(
-    new LoggerConfiguration()
-      .MinimumLevel.Debug()
-      .WriteTo.Console(formatProvider: CultureInfo.InvariantCulture)
-      .CreateLogger()
-  );
+  .SetMinimumLevel(level: LogLevel.Debug)
+  .AddSimpleConsole(options =>
+  {
+    options.IncludeScopes = false;
+    options.SingleLine = true;
+    options.UseUtcTimestamp = true;
+  });
 
 builder
   .Services.AddDbContextFactory<DataContext>()
